88603a4ccc9ffe8d5b52dde8670530c12689a048
IDenra <denrazv@mail.ru>
2021-08-25 12:39:21 +0300

    urlpatterns contains media and static files

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -20,4 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
