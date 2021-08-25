898ddd544ca4bde07e617e61dbf1827854984d15
IDenra <denrazv@mail.ru>
2021-08-25 12:31:03 +0300

    STATIC_ROOT is changed and static is added to urls

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
