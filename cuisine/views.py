72758b59e45f44f196f92dfef7201e36536d9c9e
multipassport <mashross@gmail.com>
2021-08-25 18:36:45 +0300

    Show menu for a next week

--- cuisine/views.py
+++ cuisine/views.py
@@ -1,3 +1,39 @@
+import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Meal
+
+
+def show_next_week_menu(request):
+    user_id = 1  #TODO get customer
+    weekdays = [
+        datetime.date.today() + datetime.timedelta(days=day) for day in range(1, 3)
+    ]
+
+    meals = (
+        Meal.objects
+        .filter(date__in=weekdays)
+        .filter(customer=user_id)
+        .prefetch_related('meal_positions__dish')
+    )
+
+    meals_per_day = ((day, meals.filter(date=day)) for day in weekdays)
+
+    serialized_meals = {}
+    for day, daily_serving in meals_per_day:
+        daily_dishes = {
+            'breakfast': daily_serving.get(meal_type='BREAKFAST'),
+            'lunch': daily_serving.get(meal_type='LUNCH'),
+            'dinner': daily_serving.get(meal_type='DINNER'),
+        }
+        serialized_meals.setdefault(day, daily_dishes)
+
+    return render(
+        request, 'temp_week_menu.html',
+        context={'meals': serialized_meals}
+    )
+
+
+# def calculate_products(request):
--- foodplan/urls.py
+++ foodplan/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from cuisine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('week_menu/', views.show_next_week_menu),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
