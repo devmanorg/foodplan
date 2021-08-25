51c52c50e0ded9e092a956ea55cb76e9e9239306
IDenra <denrazv@mail.ru>
2021-08-25 12:09:42 +0300

    pythonanywhere host is added to ALLOWED_HOSTS

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6m2=(1i4_+@xvjdhkdqf#q@j5t5*!o=lz@_!tnmod&o7%skg1a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foodplanlike.pythonanywhere.com']
 
 
 # Application definition
