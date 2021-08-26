f98bdaf182a6a96e7cd78f7533f0a2375b4d1249
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-08-27 10:13:52 +1100

    Fix broken registration

--- cuisine/views.py
+++ cuisine/views.py
@@ -120,17 +120,13 @@ def show_daily_menu(request):
 
 
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-
-            return redirect('index')
+            return render(request, 'register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'register.html', {'user_form': user_form})
