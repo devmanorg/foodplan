f682af47793e3db4302933bd63627df2bc4fdd94
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-09-01 14:02:58 +1100

    Remove mandatory login

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'global_login_required.GlobalLoginRequiredMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
